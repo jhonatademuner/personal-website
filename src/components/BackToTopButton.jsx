648b825024a94454bbfde3ab069e406a122e3418
Jhonata Polito <jhonata.demuner@gmail.com>
2023-08-12 01:14:29 -0300

    Update BackToTopButton.jsx

--- src/components/BackToTopButton.jsx
+++ src/components/BackToTopButton.jsx
@@ -3,7 +3,7 @@ import { BiArrowToTop } from "react-icons/bi";
 function BackToTopButton() {
   return (
     <button className="absolute bg-palette-600 rounded-full h-16 w-16 top-[-2rem] right-4 flex justify-center items-center">
-      <a href="#header">
+      <a href="#">
         <BiArrowToTop className="text-palette-100 text-4xl text-center align-middle" />
       </a>
     </button>
