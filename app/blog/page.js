71c0c3cf546ab19cee121c76ed134534fe120920
Jhonata Polito <jhonata.demuner@gmail.com>
2023-09-25 19:56:00 -0300

    Update page.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -2,10 +2,11 @@ import Image from "next/image";
 
 import Logo from "@/public/logo.svg";
 import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-w-full">
+    <main className="flex flex-col min-w-full ">
       <Navbar />
       <section className="flex flex-col min-h-screen pt-52 mx-auto xl:w-[1024px] relative">
         <a href="/">
@@ -21,6 +22,7 @@ export default function Home() {
           <p className="text-4xl text-accent font-bold">NOTHING HERE, COME BACK LATER...</p>
         </div>
       </section>
+      <Footer />
     </main>
   );
 }
