dae1f7882b1be6fade4e81c914b2dc43d456ed6c
jhonatademuner <jhonata.demuner@gmail.com>
2024-07-30 19:15:23 -0300

    remove old files

--- components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return (
-        <footer className="flex w-full justify-center items-center text-center py-8">
-            <p className="text-secondary">© 2023 Jhonata Demuner. Todos os direitos reservados.</p>
-        </footer>
-    )
-}
-
-export default Footer;
