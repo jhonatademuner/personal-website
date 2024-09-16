994915b5704321b51c0509900491f0c1e86855fc
jhonatademuner <jhonata.demuner@gmail.com>
2024-09-15 21:04:02 -0300

    fix: redirect to external tab

--- src/components/SocialMediaBar.tsx
+++ src/components/SocialMediaBar.tsx
@@ -8,22 +8,22 @@ const SocialMediaBar = () => {
       <div className="bg-background-2 h-0.5 w-full"></div>
       <div className="flex justify-center items-center pt-3">
         <div className="flex justify-center gap-4 border-2 border-background-2 rounded-full px-4 text-2xl">
-          <a href="https://discord.com/users/917529601520066560">
+          <a href="https://discord.com/users/917529601520066560" target="_blank">
             <div className="h-10 w-10 rounded-full bg-text flex justify-center items-center transition-transform transform hover:scale-110">
               <FaDiscord />
             </div>
           </a>
-          <a href="https://github.com/jhonatademuner">
+          <a href="https://github.com/jhonatademuner" target="_blank">
             <div className="h-10 w-10 rounded-full bg-text flex justify-center items-center transition-transform transform hover:scale-110">
               <FaGithub />
             </div>
           </a>
-          <a href="https://www.linkedin.com/in/jhonatademuner/">
+          <a href="https://www.linkedin.com/in/jhonatademuner/" target="_blank">
             <div className="h-10 w-10 rounded-full bg-text flex justify-center items-center transition-transform transform hover:scale-110">
               <FaLinkedin />
             </div>
           </a>
-          <a href="mailto:jhonata.demuner@gmail.com">
+          <a href="mailto:jhonata.demuner@gmail.com" target="_blank">
             <div className="h-10 w-10 rounded-full bg-text flex justify-center items-center transition-transform transform hover:scale-110">
               <IoMdMail />
             </div>
